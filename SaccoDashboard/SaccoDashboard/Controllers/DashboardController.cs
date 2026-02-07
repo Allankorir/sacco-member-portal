12f97bd3df788a5c2fa468618e351f7712e32e84
Allankorir <ak@Flowpoint.dk>
2026-02-07 18:47:49 +0300

    Initial commit: Member Portal dashboard UI

---
+++ SaccoDashboard/SaccoDashboard/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaccoDashboard.Models;
+
+namespace SaccoDashboard.Controllers;
+
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        var vm = new DashboardVm
+        {
+            CurrentSavings = 15450.75m,
+            LoanTotal = 154000m,
+            LoanRemaining = 3200m,
+            DailyLabels = new() { "30", "80", "81", "60", "50", "80", "20" },
+            DailyContributions = new() { 50, 120, 900, 450, 700, 800, 1200 },
+            RecentTransactions = new()
+            {
+                new() { DateLabel="LAST/2017", Description="Contice Mection", Amount=2500.00m },
+                new() { DateLabel="LAST/2017", Description="Conte Band Artiction", Amount=3000.00m },
+                new() { DateLabel="LAST/2018", Description="Corttion Nerar", Amount=3700.00m },
+            }
+        };
+
+        return View(vm);
+    }
+}
---
+++ SaccoDashboard/SaccoDashboard/Models/DashboardVm.cs
@@ -0,0 +1,18 @@
+﻿namespace SaccoDashboard.Models;
+
+public class DashboardVm
+{
+    public decimal CurrentSavings { get; set; }
+    public decimal LoanTotal { get; set; }
+    public decimal LoanRemaining { get; set; }
+    public List<decimal> DailyContributions { get; set; } = new();
+    public List<string> DailyLabels { get; set; } = new();
+    public List<TransactionVm> RecentTransactions { get; set; } = new();
+}
+
+public class TransactionVm
+{
+    public string DateLabel { get; set; } = "";
+    public string Description { get; set; } = "";
+    public decimal Amount { get; set; }
+}
